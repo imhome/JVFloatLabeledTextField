1543707d7ffcef91a971ffd335a9e91e22f7b79f
Kevin Xu <kevin@imkevinxu.com>
2015-06-12 02:28:02 -0700

    Removed IB_DESIGNABLE and IBInspectable

--- JVFloatLabeledTextField/JVFloatLabeledTextField/JVFloatLabeledTextField.h
+++ JVFloatLabeledTextField/JVFloatLabeledTextField/JVFloatLabeledTextField.h
@@ -33,14 +33,13 @@
  * Due to space constraints on mobile devices, it is common to rely solely on placeholders as a means to label fields.
  * This presents a UX problem, in that, once the user begins to fill out a form, no labels are present.
  *
- * `JVFloatLabeledTextField` aims to improve the user experience by having placeholders transition into 
+ * `JVFloatLabeledTextField` aims to improve the user experience by having placeholders transition into
  * "floating labels" that hover above the text field after it is populated with text.
  *
  * JVFloatLabeledTextField supports iOS 6+.
  *
  * Credits for the concept to Matt D. Smith (@mds), and his original design:  http://mattdsmith.com/float-label-pattern/
  */
-IB_DESIGNABLE
 @interface JVFloatLabeledTextField : UITextField
 
 /**
@@ -52,22 +51,22 @@ IB_DESIGNABLE
  * Padding to be applied to the y coordinate of the floating label upon presentation.
  * Defaults to zero.
  */
-@property (nonatomic) IBInspectable CGFloat floatingLabelYPadding;
+@property (nonatomic) CGFloat floatingLabelYPadding;
 
 /**
  * Padding to be applied to the x coordinate of the floating label upon presentation.
  * Defaults to zero
  */
-@property (nonatomic) IBInspectable CGFloat floatingLabelXPadding;
+@property (nonatomic) CGFloat floatingLabelXPadding;
 
 /**
  * Padding to be applied to the y coordinate of the placeholder.
  * Defaults to zero.
  */
-@property (nonatomic) IBInspectable CGFloat placeholderYPadding;
+@property (nonatomic) CGFloat placeholderYPadding;
 
 /**
- * Font to be applied to the floating label. 
+ * Font to be applied to the floating label.
  * Defaults to the first applicable of the following:
  * - the custom specified attributed placeholder font at 70% of its size
  * - the custom specified textField font at 70% of its size
@@ -75,31 +74,31 @@ IB_DESIGNABLE
 @property (nonatomic, strong) UIFont * floatingLabelFont;
 
 /**
- * Text color to be applied to the floating label. 
+ * Text color to be applied to the floating label.
  * Defaults to `[UIColor grayColor]`.
  */
-@property (nonatomic, strong) IBInspectable UIColor * floatingLabelTextColor;
+@property (nonatomic, strong) UIColor * floatingLabelTextColor;
 
 /**
  * Text color to be applied to the floating label while the field is a first responder.
  * Tint color is used by default if an `floatingLabelActiveTextColor` is not provided.
  */
-@property (nonatomic, strong) IBInspectable UIColor * floatingLabelActiveTextColor;
+@property (nonatomic, strong) UIColor * floatingLabelActiveTextColor;
 
 /**
  * Indicates whether the floating label's appearance should be animated regardless of first responder status.
  * By default, animation only occurs if the text field is a first responder.
  */
-@property (nonatomic, assign) IBInspectable BOOL animateEvenIfNotFirstResponder;
+@property (nonatomic, assign) BOOL animateEvenIfNotFirstResponder;
 
 /**
- * Duration of the animation when showing the floating label. 
+ * Duration of the animation when showing the floating label.
  * Defaults to 0.3 seconds.
  */
 @property (nonatomic, assign) NSTimeInterval floatingLabelShowAnimationDuration;
 
 /**
- * Duration of the animation when hiding the floating label. 
+ * Duration of the animation when hiding the floating label.
  * Defaults to 0.3 seconds.
  */
 @property (nonatomic, assign) NSTimeInterval floatingLabelHideAnimationDuration;
@@ -108,13 +107,13 @@ IB_DESIGNABLE
  * Indicates whether the clearButton position is adjusted to align with the text
  * Defaults to 1.
  */
-@property (nonatomic, assign) IBInspectable BOOL adjustsClearButtonRect;
+@property (nonatomic, assign) BOOL adjustsClearButtonRect;
 
 /**
  * Indicates whether or not to drop the baseline when entering text. Setting to YES (not the default) means the standard greyed-out placeholder will be aligned with the entered text
  * Defaults to NO (standard placeholder will be above whatever text is entered)
  */
-@property (nonatomic, assign) IBInspectable BOOL keepBaseline;
+@property (nonatomic, assign) BOOL keepBaseline;
 
 /**
  *  Sets the placeholder and the floating title
